53eba3bd8afe02051cc9766dc2296125f7309899
moutaz3003 <moutaz30@hotmail.com>
2021-09-07 12:33:28 +0000

    Create signup.html template, add styling to signup form and connect form to signup view in app.py file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash 
 if os.path.exists("env.py"):
     import env
 
@@ -24,6 +25,11 @@ mongo = PyMongo(app)
 def home():
     recipes = mongo.db.recipes.find()
     return render_template("index.html", recipes=recipes)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
 
 
 @app.route("/find_recipes")
