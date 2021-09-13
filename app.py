2e5c8476fd86c7acb330c9edc5474a808789b849
moutaz3003 <moutaz30@hotmail.com>
2021-09-13 19:04:56 +0000

    Add delete functionality to delete user submitted recipe

--- app.py
+++ app.py
@@ -168,6 +168,14 @@ def edit_recipe(recipe_id):
     return render_template("edit_recipe.html", recipe=recipe, categories=categories)
 
 
+#------------- Delete Recipe -----------------------
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    flash("Recipe Has Been Deleted")
+    return redirect(url_for("profile", username=session["user"]))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
