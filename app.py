bb7fd94799b25a9bc617ff3af9055fa7c61d295c
moutaz3003 <moutaz30@hotmail.com>
2021-09-17 05:25:10 +0000

    Add proper commenting on python routes and provide further elaboration where relevant

--- app.py
+++ app.py
@@ -20,17 +20,17 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-#------------- Home -----------------------
 @app.route("/")
 def home():
+    """------------- Home -----------------------"""
     recipes = list(mongo.db.recipes.find())
     recipe_tabs = list(mongo.db.recipes.find())
     return render_template("index.html", recipes=recipes, recipe_tabs=recipe_tabs)
 
 
-#------------- Sign Up -----------------------
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    """------------- Sign Up -----------------------"""
     if request.method == "POST":
         #Check if the username already exists
         existing_user = mongo.db.users.find_one(
@@ -53,9 +53,9 @@ def signup():
     return render_template("signup.html")
 
 
-#------------- Log in -----------------------
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    """------------- Log in -----------------------"""
     if request.method == "POST":
         #check if the username already exists in the database
         existing_user = mongo.db.users.find_one(
@@ -81,9 +81,9 @@ def login():
     return render_template("login.html")
 
 
-#------------- User profile page -----------------------
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
+    """------------- User profile page -----------------------"""
     #retrieve the session user's username from the database
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
@@ -97,64 +97,67 @@ def profile(username):
     return redirect(url_for("login"))
 
 
-#------------- Log out -----------------------
 @app.route("/logout")
 def logout():
+    """------------- Log out -----------------------"""
     #remove user session cookies
     flash("You Have Been Logged Out")
     session.pop("user")
     return redirect(url_for("login"))
 
 
-#------------ Meat alternatives------------------
 @app.route("/meat_alternatives")
 def meat_alternatives():
+    """------------ Meat alternatives route ------------------"""
     return render_template("meat_alternatives.html")
 
 
-#------------ Milk alternatives------------------
 @app.route("/milk_alternatives")
 def milk_alternatives():
+    """------------ Milk alternatives------------------"""
     return render_template("milk_alternatives.html")
 
 
-#------------ Egg alternatives------------------
 @app.route("/egg_alternatives")
 def egg_alternatives():
+    """------------ Egg alternatives------------------"""
     return render_template("egg_alternatives.html")
 
 
-#------------- Recipes page -----------------------
 @app.route("/find_recipes")
 def find_recipes():
+    """------------- Search All Recipes page -----------------------"""
     recipes = list(mongo.db.recipes.find())
     return render_template("find_recipes.html", recipes=recipes)
 
 
-#------------ Search Recipe ------------------------------
 @app.route("/search", methods=["GET", "POST"])
 def search():
+    """--------- Search Recipe view for find_recipes search query --------"""
     query = request.form.get("query")
     recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
     return render_template("find_recipes.html", recipes=recipes)
 
 
-#------------- Individual recipe -----------------------
 @app.route("/recipe/<recipe_id>")
 def single_recipe(recipe_id):
+    """------------- View Individual recipe -----------------------"""
     recipe = mongo.db.recipes.find_one_or_404({"_id": ObjectId(recipe_id)})
     return render_template("single_recipe.html", recipe=recipe)
 
 
-#------------- Add Recipe -----------------------
 @app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
+    """------------- Add Recipe -----------------------"""
     categories = mongo.db.categories.find().sort("category_name", 1)
 
     if request.method == "POST":
         form_info = request.form.to_dict()
+        # Split text area content on new line for ingredients and instructions
         recipe_ingredients_list = form_info["recipe_ingredients"].split("\n")
         recipe_method_list = form_info["recipe_method"].split("\n")
+        
+        #get recipe info from user input fields
         new_recipe = {
             "category_name": form_info["category_name"],
             "recipe_title": form_info["recipe_title"],
@@ -174,18 +177,20 @@ def add_recipe():
     return render_template("add_recipe.html", categories=categories)
 
 
-#------------- Edit Recipe -----------------------
 @app.route("/edit_recipe/<recipe_id>")
 def edit_recipe(recipe_id):
+    """------------ Edit Recipe -----------------"""
     recipe = mongo.db.recipes.find_one_or_404({"_id": ObjectId(recipe_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     
+    #create lists for ingredients and method
     recipe_ingredients_list = [ingredient for ingredient
                                 in recipe['recipe_ingredients']]
     
     recipe_instructions_list = [instruction for instruction
                                 in recipe['recipe_method']]
     
+    #join list on new line
     ingredients_text = "\n".join(recipe_ingredients_list)
     instructions_text = "\n".join(recipe_instructions_list)
     flash("Recipe Successfully Updated")
@@ -196,9 +201,9 @@ def edit_recipe(recipe_id):
                             instructions=instructions_text)
 
 
-#------------- Delete Recipe -----------------------
 @app.route("/delete_recipe/<recipe_id>")
 def delete_recipe(recipe_id):
+    """------------- Delete Recipe ---------------"""
     mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
     flash("Recipe Has Been Deleted")
     return redirect(url_for("profile", username=session["user"]))
