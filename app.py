d3d5450b8b7d57d3dda0073d1a943f491380871f
moutaz3003 <moutaz30@hotmail.com>
2021-09-17 09:45:49 +0000

    Format python file and check for PEP8 compliance

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
-from flask import (
-    Flask, flash, render_template, 
-    redirect, request, session, url_for)
+from flask import (Flask, flash, render_template,
+                   redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 if os.path.exists("env.py"):
     import env
 
@@ -16,7 +16,7 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 
-#set up instance of PyMongo
+# set up instance of PyMongo
 mongo = PyMongo(app)
 
 
@@ -25,16 +25,18 @@ def home():
     """------------- Home -----------------------"""
     recipes = list(mongo.db.recipes.find())
     recipe_tabs = list(mongo.db.recipes.find())
-    return render_template("index.html", recipes=recipes, recipe_tabs=recipe_tabs)
+    return render_template("index.html",
+                           recipes=recipes, recipe_tabs=recipe_tabs)
 
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     """------------- Sign Up -----------------------"""
     if request.method == "POST":
-        #Check if the username already exists
+        # Check if the username already exists
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+            {"username": request.form.get("username").lower()}
+        )
 
         if existing_user:
             flash("Username already exists")
@@ -42,11 +44,11 @@ def signup():
 
         register = {
             "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
+            "password": generate_password_hash(request.form.get("password")),
         }
         mongo.db.users.insert_one(register)
 
-        #put the new user in a session cookie
+        # put the new user in a session cookie
         session["user"] = request.form.get("username").lower()
         flash("Sign Up Successful!")
         return redirect(url_for("profile", username=session["user"]))
@@ -57,42 +59,44 @@ def signup():
 def login():
     """------------- Log in -----------------------"""
     if request.method == "POST":
-        #check if the username already exists in the database
+        # check if the username already exists in the database
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+            {"username": request.form.get("username").lower()}
+        )
 
         if existing_user:
-            #check to see if hashed password matches user input
+            # check to see if hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+                existing_user["password"], request.form.get("password")
+            ):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome {}".format(request.form.get("username")))
                 return redirect(url_for("profile", username=session["user"]))
             else:
-                #Invalid password match
+                # Invalid password match
                 flash("Invalid Username and/or Password")
                 return redirect(url_for("login"))
-        
+
         else:
-            #Username does not exist
+            # Username does not exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
-                               
+
     return render_template("login.html")
 
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     """------------- User profile page -----------------------"""
-    #retrieve the session user's username from the database
+    # retrieve the session user's username from the database
     username = mongo.db.users.find_one(
-        {"username": session["user"]})["username"]
-    
+               {"username": session["user"]})["username"]
+
     recipes = mongo.db.recipes.find()
 
     if session["user"]:
-        return render_template("profile.html", 
-        username=username, recipes=recipes)
+        return render_template("profile.html",
+                               username=username, recipes=recipes)
 
     return redirect(url_for("login"))
 
@@ -100,7 +104,7 @@ def profile(username):
 @app.route("/logout")
 def logout():
     """------------- Log out -----------------------"""
-    #remove user session cookies
+    # remove user session cookies
     flash("You Have Been Logged Out")
     session.pop("user")
     return redirect(url_for("login"))
@@ -156,8 +160,8 @@ def add_recipe():
         # Split text area content on new line for ingredients and instructions
         recipe_ingredients_list = form_info["recipe_ingredients"].split("\n")
         recipe_method_list = form_info["recipe_method"].split("\n")
-        
-        #get recipe info from user input fields
+
+        # get recipe info from user input fields
         new_recipe = {
             "category_name": form_info["category_name"],
             "recipe_title": form_info["recipe_title"],
@@ -168,8 +172,8 @@ def add_recipe():
             "recipe_ingredients": recipe_ingredients_list,
             "recipe_method": recipe_method_list,
             "recipe_image_url": form_info["recipe_image_url"],
-            "created_by": session["user"]
-            }
+            "created_by": session["user"],
+        }
         mongo.db.recipes.insert_one(new_recipe)
         flash("Recipe Successfully Added")
         return redirect(url_for("add_recipe"))
@@ -182,23 +186,26 @@ def edit_recipe(recipe_id):
     """------------ Edit Recipe -----------------"""
     recipe = mongo.db.recipes.find_one_or_404({"_id": ObjectId(recipe_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
-    
-    #create lists for ingredients and method
-    recipe_ingredients_list = [ingredient for ingredient
-                                in recipe['recipe_ingredients']]
-    
+
+    # create lists for ingredients and method
+    recipe_ingredients_list = [
+        ingredient for ingredient in recipe["recipe_ingredients"]
+    ]
+
     recipe_instructions_list = [instruction for instruction
-                                in recipe['recipe_method']]
-    
-    #join list on new line
+                                in recipe["recipe_method"]]
+
+    # join list on new line
     ingredients_text = "\n".join(recipe_ingredients_list)
     instructions_text = "\n".join(recipe_instructions_list)
     flash("Recipe Successfully Updated")
-    return render_template("edit_recipe.html", 
-                            recipe=recipe, 
-                            categories=categories, 
-                            ingredients=ingredients_text,
-                            instructions=instructions_text)
+    return render_template(
+        "edit_recipe.html",
+        recipe=recipe,
+        categories=categories,
+        ingredients=ingredients_text,
+        instructions=instructions_text,
+    )
 
 
 @app.route("/delete_recipe/<recipe_id>")
@@ -211,5 +218,4 @@ def delete_recipe(recipe_id):
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+            port=int(os.environ.get("PORT")), debug=True)
